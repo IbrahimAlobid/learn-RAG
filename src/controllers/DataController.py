bb230b7b295678bb55477dabdf474f7865edcb17
IbrahimAlobid <ibrahim.alobid44@gmail.com>
2025-01-04 20:59:25 +0300

    branch 4 section 2

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,7 +1,9 @@
 from .BaseController import BaseController
 from fastapi import UploadFile
-
-
+from models import ResponseSignal
+import re
+from .ProjectContoller import ProjectController
+import os 
 class DataController(BaseController):
     
     def __init__(self):
@@ -9,13 +11,50 @@ class DataController(BaseController):
         self.size_scale = 1048576
         
     
+    
+    
     def validate_uploaded_file(self , file: UploadFile):
         
         if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES:
-            return False ,'file type not suppurt'
+            return False ,ResponseSignal.FAILE_TYPE_NOT_SPPORTED.value
         
         if file.size > self.app_settings.FILE_MAX_SIZE * self.size_scale:
-            return False,"file size exeeded"
-        
-        return True ,'success'
+            return False,ResponseSignal.FILE_SIZE_EXCEEDED.value
+        return True ,ResponseSignal.FILE_UPLOAD_SUCCESS.value
     
+    
+    
+    def generate_unique_filename(self , orig_file_name : str ,project_id :str ):
+        random_keye = self.generate_random_string()
+        
+        project_path =ProjectController().get_project_path(project_id=project_id)
+        
+        cleaned_file_name = self.get_clean_file_name(
+            orig_file_name =orig_file_name
+            )
+        
+        new_file_path = os.path.join(
+            project_path ,
+            random_keye + '_'+cleaned_file_name
+        )
+        
+        while os.path.exists(new_file_path):
+             random_keye = self.generate_random_string()
+             new_file_path = os.path.join(
+                    project_path ,
+                    random_keye + '_'+cleaned_file_name
+                )
+        return new_file_path , random_keye + '_'+cleaned_file_name
+            
+        
+    def get_clean_file_name(self, orig_file_name: str):
+
+        # remove any special characters, except underscore and .
+        cleaned_file_name = re.sub(r'[^\w.]', '', orig_file_name.strip())
+
+        # replace spaces with underscore
+        cleaned_file_name = cleaned_file_name.replace(" ", "_")
+
+        return cleaned_file_name
+
+
--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,1 +1,2 @@
 from .DataController import DataController
+from .ProjectContoller import ProjectController
