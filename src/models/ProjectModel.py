3adf26d29dd5e4981a46c5f8bfcdc6da39d84e8d
IbrahimAlobid <ibrahim.alobid44@gmail.com>
2025-01-12 11:33:05 +0300

    create indexes dor mogodb

--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -7,7 +7,25 @@ class ProjectModel(BaseDataModel):
     def __init__(self, db_client: object):
         super().__init__(db_client=db_client)
         self.collection = self.db_client[DataBaseEnum.COLLECTION_PROJECT_NAME.value]
-
+        
+        
+    @classmethod    
+    async def create_instanse(cls ,db_client: object ):
+        instanse =cls(db_client)
+        await instanse.init_collection()
+        return instanse
+    async def init_collection(self):
+        all_collections =await self.db_client.list_collection_names()
+        if DataBaseEnum.COLLECTION_PROJECT_NAME.value not in all_collections :
+            self.collection = self.db_client[DataBaseEnum.COLLECTION_PROJECT_NAME.value]
+            indexes = Project.get_indexes()
+            for index in indexes:
+                await self.collection.create_index(
+                    index["key"],
+                    name = index["name"],
+                    unique = index['unique']
+                )
+             
     async def create_project(self, project: Project):
 
         result = await self.collection.insert_one(project.dict(by_alias=True, exclude_unset=True))
